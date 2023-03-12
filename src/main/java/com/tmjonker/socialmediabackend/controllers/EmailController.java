436ce8f3e8fe8c0cd81307e705b101fee4483a53
tmjonker <tmjonker1@outlook.com>
2023-03-12 19:44:15 -0400

    Added DTO classes for email and username, and fixed bug that would not allow data to be passed to controllers.

--- src/main/java/com/tmjonker/socialmediabackend/controllers/EmailController.java
+++ src/main/java/com/tmjonker/socialmediabackend/controllers/EmailController.java
@@ -1,11 +1,15 @@
 package com.tmjonker.socialmediabackend.controllers;
 
+import com.tmjonker.socialmediabackend.dto.EmailDTO;
 import com.tmjonker.socialmediabackend.services.CustomUserDetailsService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @CrossOrigin
 @RestController
@@ -19,10 +23,9 @@ public class EmailController {
     }
 
     @PostMapping("/email")
-    public ResponseEntity<?> checkEmailExists(String email) {
+    public ResponseEntity<?> checkEmailExists(@RequestBody EmailDTO email) {
 
-        boolean exists = userDetailsService.userExistsByEmail(email);
-
+        boolean exists = userDetailsService.userExistsByEmail(email.getEmail());
         if (!exists) {
             return new ResponseEntity<>(HttpStatus.OK);
         } else {
--- src/main/java/com/tmjonker/socialmediabackend/controllers/UsernameController.java
+++ src/main/java/com/tmjonker/socialmediabackend/controllers/UsernameController.java
@@ -1,10 +1,16 @@
 package com.tmjonker.socialmediabackend.controllers;
 
+import com.tmjonker.socialmediabackend.dto.UsernameDTO;
 import com.tmjonker.socialmediabackend.services.CustomUserDetailsService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
+@RestController
 public class UsernameController {
 
     private CustomUserDetailsService userDetailsService;
@@ -15,9 +21,9 @@ public class UsernameController {
     }
 
     @PostMapping("/username")
-    public ResponseEntity<?> checkUsernameExists(String username) {
+    public ResponseEntity<?> checkUsernameExists(@RequestBody UsernameDTO username) {
 
-        boolean exists = userDetailsService.userExistsByUsername(username);
+        boolean exists = userDetailsService.userExistsByUsername(username.getUsername());
 
         if (!exists) {
             return new ResponseEntity<>(HttpStatus.OK);
